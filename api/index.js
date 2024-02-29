ed721397073cd9985d6b82a054cdc00b3f542fa8
Vandan Sheth <Vandanbsheth9@gmail.com>
2024-02-29 18:38:20 +0530

    connecting to Mongodb

--- api/index.js
+++ api/index.js
@@ -1,8 +1,17 @@
 import express from 'express';
+import mongoose from 'mongoose';
+// import dotenv from 'dotenv';
+// dotenv.config();
+
+mongoose.connect("mongodb+srv://vandan:vandan@real-estate.ecxgbbb.mongodb.net/real-estate?retryWrites=true&w=majority&appName=real-estate")
+.then(()=>{
+    console.log("Connected to Database");
+})
+.catch(err => {console.error(err)});
 
 const app = express();
 
 app.listen(3000, () => {
-    console.log("Server is running !!");
+    console.log("Server is running on port 3000 !!");
     }
 );
