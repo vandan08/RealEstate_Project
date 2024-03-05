3794e6a116ddb103b79699a40dc310cfd1ff3e5f
Vandan Sheth <Vandanbsheth9@gmail.com>
2024-03-05 12:16:16 +0530

    update user api route

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,5 +1,43 @@
+import bcryptjs from 'bcryptjs';
+import User from '../models/user.model.js';
+import { errorHandler } from '../utils/error.js';
+
+
 export const test = (req, res) => {
   res.json({
     message: 'APi'
   })
+  };
+
+  //this function  is used to get all the updated data of user and change it in database 
+  export const updateUser = async (req, res, next) => {
+    //if the user id is not same as the id which pass throgh  url then return an error massage
+    if (req.user.id !== req.params.id)
+      return next(errorHandler(401, 'You can only update your own account!'));
+    try {
+      //if user wants to change password 
+      if (req.body.password) {
+        req.body.password = bcryptjs.hashSync(req.body.password, 10);
+      }
+      //update the user data in the database , 'data' getting from body 
+      const updatedUser = await User.findByIdAndUpdate( 
+        // findByIdAndUpdate is an mongodb function to find the user by it's id and update the user's data 
+        req.params.id,
+        {
+          $set: {
+            username: req.body.username,
+            email: req.body.email,
+            password: req.body.password,
+            avatar: req.body.avatar,
+          },
+        },
+        { new: true }
+      );
+  
+      const { password, ...rest } = updatedUser._doc;
+  
+      res.status(200).json(rest);
+    } catch (error) {
+      next(error);
+    }
   };
--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ import mongoose from 'mongoose';
 import userRouter from './routes/user.route.js';
 import  authRouter from './routes/auth.route.js'
 import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
 // require("dotenv").config();
 dotenv.config(); 
 
@@ -14,6 +15,7 @@ mongoose.connect("mongodb+srv://vandan:vandan@real-estate.ecxgbbb.mongodb.net/re
 
 const app = express();
 app.use(express.json());
+app.use(cookieParser());
 
 app.listen(3000, () => {
     console.log("Server is running on port 3000 !!");
